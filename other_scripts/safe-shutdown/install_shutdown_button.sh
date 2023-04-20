6e263236a9b2e1332167ec4108bdf8b1cd4d3390
R1oby <system.manager@gmail.com>
2023-04-20 12:17:14 +0700

    removed strange name and moved to /opt/bin/
    removed strange name and moved to /opt/bin/

---
+++ other_scripts/safe-shutdown/install_shutdown_button.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+clear
+cat <<EOF
+#######################################################
+#######################################################
+
+Connect the button to:
+BCM23 (pin 16) & GND (pins 9, 14, 20, 25, 30, 34, 39).
+
+Incorrect wiring of the Raspberry Pi GPIO pins
+can cause permanent damage to your Pi. You assume
+all responsibility when wiring your Pi.
+#######################################################
+#######################################################
+EOF
+
+read -n 1 -s -r -p "Press any key to continue"
+clear
+echo;echo "installing safe shutdown script"
+
+###     sudo mv $MYDIR/km4ack-shutdown /usr/local/bin/
+###     sudo mv $MYDIR/km4ack-shutdown.service /lib/systemd/system/
+
+
+sudo cp $MYDIR/shutdown_button.sh /opt/bin/
+sudo cp $MYDIR/shutdown_button.service /lib/systemd/system/
+
+
+echo "enabling script on boot"
+
+### sudo systemctl enable km4ack-shutdown.service
+### sudo systemctl start km4ack-shutdown.service
+
+sudo systemctl enable shutdown_button.service
+sudo systemctl start shutdown_button.service
+
+
+#get status of service
+### STATUS=$(sudo systemctl is-enabled km4ack-shutdown.service)
+STATUS=$(sudo systemctl is-enabled shutdown_button.service)
+
+
+cat <<EOF
+
+
+Script installed and $STATUS
+
+#######################################################
+#######################################################
+
+Connect the button to:
+BCM23 (pin 16) & GND (pins 9, 14, 20, 25, 30, 34, 39).
+
+#######################################################
+#######################################################
+
+To disable the script from running at boot, run:
+sudo systemctl disable shutdown_button.service
+
+To enable the script to run at boot, run:
+sudo systemctl enable shutdown_button.service
+
+EOF
